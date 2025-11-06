9d3382abea85c45cc60936e2e0f004e6e262f212
RodSarhan <46911303+RodSarhan@users.noreply.github.com>
2025-11-06 23:16:40 +0800

    chore: update release-it configs

--- .release-it.ts
+++ .release-it.ts
@@ -3,6 +3,6 @@ import type {Config} from 'release-it';
 export default {
     git: {commitMessage: 'chore: release ${version}', tagName: 'v${version}'},
     npm: {publish: true},
-    github: {release: true},
+    github: {release: true, web: true},
     plugins: {'@release-it/conventional-changelog': {preset: 'angular'}},
 } satisfies Config;
