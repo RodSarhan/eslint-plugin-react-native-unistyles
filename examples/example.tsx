af98259fc53e78d3e2a933c8d8936b5ad8030fbd
RodSarhan <46911303+RodSarhan@users.noreply.github.com>
2025-11-08 21:35:52 +0800

    add examples

--- examples/example.tsx
+++ examples/example.tsx
@@ -1,9 +1,19 @@
 import {StyleSheet} from 'react-native-unistyles';
 import {View, Text} from 'react-native';
 
-const MyComponent = wrapper(() => {
-    return <Text style={styles.foo}>Hello</Text>;
-});
+const styles = StyleSheet.create(() => ({
+    container: {flex: 1, justifyContent: 'center'},
+    foo1: {fontSize: 16},
+    unusedStyle: {color: 'red'},
+    someWrongOrderStyle: {margin: 10},
+}));
+
+const styles2 = StyleSheet.create(() => ({
+    container: {flex: 1, justifyContent: 'center'},
+    foo1: {fontSize: 16},
+    foo2: {fontSize: 16},
+    foo3: {fontSize: 16},
+}));
 
 const App = () => (
     <View style={styles.container}>
@@ -11,9 +21,16 @@ const App = () => (
     </View>
 );
 
-const styles = StyleSheet.create(() => ({
-    container: {flex: 1, justifyContent: 'center'},
-    foo: {fontSize: 16},
-    unusedStyle: {color: 'red'},
-    someWrongOrderStyle: {margin: 10},
-}));
+const MyComponent = wrapper(() => {
+    return <Text style={styles2.foo1}>Hello</Text>;
+});
+
+const MyComponent2 = wrapper2(function () {
+    return <Text style={styles2.foo2}>Hello</Text>;
+});
+
+function MyComponent3() {
+    return wrapper(function namedfunc() {
+        return <Text style={styles2.foo3}>Hello</Text>;
+    });
+}
